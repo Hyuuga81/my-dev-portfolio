5a9cf562522378ba19a863a9af4856ff4a7b4d73
Jay <j.adeyina@gmail.com>
2023-04-08 18:50:43 +0900

    Style updates

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 const bars = document.querySelector(".fa-bars");
-const close = document.querySelector(".close");
+const xmark = document.querySelector(".fa-xmark");
 const overlay = document.querySelector(".overlay");
 const link = document.querySelectorAll(".overlay-content a");
 const toTop = document.querySelector(".to-top");
 
 // Navigation menu 
 bars.addEventListener("click", function () {
-    overlay.style.width = "100%";
+        overlay.style.width = "100%";
+        bars.classList.add("hide");
+        xmark.classList.remove("hide");
 });
 
-close.addEventListener("click", function () {
-    overlay.style.width = "0%";
+xmark.addEventListener("click", function () {
+        overlay.style.width = "0%";
+        bars.classList.remove("hide");
+        xmark.classList.add("hide");
 });
 
+//Navigation links
 for (var item of link) {
     item.addEventListener("click", function () {
         overlay.style.width = "0%";
+        xmark.classList.add("hide");
+        bars.classList.remove("hide");
     })
-}
+};
 
 // To top button
 window.addEventListener("scroll", function () {
@@ -26,4 +33,4 @@ window.addEventListener("scroll", function () {
     } else {
         toTop.classList.remove("active");
     }
-})
+});
